102f5e6cd207cc0a273e029db73e20b2c246f05e
SarahChen <bamboo7988@gmail.com>
2016-06-16 10:34:07 +0800

    min changing 20160616
    add swap before compiling

--- startup_script/phalcon.sh
+++ startup_script/phalcon.sh
@@ -29,11 +29,22 @@ if [ $FORCE != "yes" ]; then
     exit;
 fi
 
+#add swap ,prevent the swap insufficient 
+mkdir -p /var/cache/swap/
+dd if=/dev/zero of=/var/cache/swap/swap0 bs=1M count=512
+chmod 0600 /var/cache/swap/swap0
+mkswap /var/cache/swap/swap0 
+swapon /var/cache/swap/swap0
+
 git clone --depth=1 git://github.com/phalcon/cphalcon.git
 
 cd cphalcon/build
 
 ./install
+
+#remove swap after installing
+swapon /var/cache/swap/swap0
+rm /var/cache/swap
 
 #reboot to check all setting
 reboot
