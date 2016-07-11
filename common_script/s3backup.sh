bffeb3300ed9c53dc5838d31daafcd5af9040a2e
sunz5010 <missyou_z@msn.com>
2016-07-11 17:27:17 +0800

    Update s3backup.sh

--- common_script/s3backup.sh
+++ common_script/s3backup.sh
@@ -12,7 +12,7 @@ export HOME=/root
 #setting
 backup_path=/home/mysqlbackup/
 backup_file=$(ls ${backup_path} -t|head -1)
-#s3path=s3://reponame/foldername
+#s3path=s3://reponame/foldername/
 
 #run backup command
 aws s3 cp ${backup_path}${backup_file} ${s3path} >> /tmp/s3backup.log
