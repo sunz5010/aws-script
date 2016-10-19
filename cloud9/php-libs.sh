85bc4770770002b8382bb83ba38719024606598d
mac fofo <sunz5010>
2016-10-19 12:11:39 +0800

    修正cloud9 php-libs裡面開啟mod部分

--- cloud9/php-libs.sh
+++ cloud9/php-libs.sh
@@ -4,3 +4,10 @@ sudo apt-get install -y php5-memcache php5-memcached libapache2-mod-php5 php5-gd
 #install mongo
 sudo pecl install mongo
 sudo pecl install mongodb
+
+#
+sudo echo "extension=mongo.so" > /etc/php5/mods-available/mongo.ini
+sudo echo "extension=mongodb.so" > /etc/php5/mods-available/mongodb.ini
+
+sudo php5enmod mongo
+sudo php5enmod mongodb
